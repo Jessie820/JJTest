31b3f674b1e2e397c873b3d0522c53318a29ecd9
Jaeyoon Lee <ljaeyoon820@gmail.com>
2018-04-29 19:07:35 +0900

    first commit

--- Algorithm/src/basic/BOJ15686.java
+++ Algorithm/src/basic/BOJ15686.java
@@ -1,9 +1,14 @@
 package basic;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
 public class BOJ15686 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
 
 	}
 
